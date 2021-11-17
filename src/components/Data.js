6b9555564471253bf1c535c9a410f59efd831af4
Sergei Palmer <zigpalmer@gmail.com>
2021-11-17 15:04:22 +0000

    Update data method

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,2 +1,25 @@
 
-const Data = ()
+import React, { useEffect } from "react";
+
+const Data = () => {
+  useEffect(() => {
+    const url = "https://the-acebook-api-test.herokuapp.com/api/v1/posts/";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json[json.length - 1].message);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
+  return <div></div>;
+};
+
+export default Data;
